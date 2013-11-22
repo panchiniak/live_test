c698f6a6e9f170672b6487779cc59c19fb8b66b5
panchiniak <rodrigo@toetec.com.br>
2013-11-21 22:48:58 -0200

    Added comments.

--- install.sh
+++ install.sh
@@ -9,68 +9,70 @@ echo 'L           i     v  v   e                 T     e       s    s     t   '
 echo 'LLLLLLL     i      vv    eeeeee            T     eeeeee   ssss      t   '
 echo -e '\n\nAutomatic installer\n'
 
-if [ "$UID" -ne "0"  ] 
-  then
-  echo 'You must be root to install Live Test !'
-  exit
-fi
+echo $1
 
-## BEGIN PROGRAM
-
-echo "Type the absolut path of the project's root folder:"
-read PROJECT_ROOT
-
-cd $PROJECT_ROOT
-
-if [ -d "${PWD}/tests/lib" ]; then
-  echo 'You already have Live Test installed. Be happy: updating requirements...'  
-else
-  echo -e 'Downloading Live Tests...\n'
-  wget http://www.toetec.com.br/live_test.tar.gz  
-  echo -e 'Unpacking the file...\n'
-  tar -xzvf live_test.tar.gz  
-  echo -e 'Installing requirements...\n'
-fi
-
-cd tests
-
-apt-get update &> /dev/null
-
-hash ruby1.9.3 &> /dev/null || {
-	echo '+ Installing Ruby1.9.3'
-	apt-get install -y ruby1.9.3 > /dev/null
-}
-
-hash xvfb &> /dev/null || {
-	echo '+ Installing xvfb'
-	apt-get install -y xvfb > /dev/null
-}
-
-hash firefox &> /dev/null || {
-	echo '+ Installing Firefox'
-	apt-get install -y firefox > /dev/null
-}
-
-hash make &> /dev/null || {
-	echo '+ Installing Firefox'
-	apt-get install -y make > /dev/null
-}
-
-hash bundler &> /dev/null || {
-	echo '+ Installing bundler'
-	gem install bundler  > /dev/null
-}
-
-hash bundle &> /dev/null || {
-	echo '+ Installing required gems...'
-	bundle install  > /dev/null
-}
-
-hash rllangs &> /dev/null || {
-	echo '+ Setting i18n support...'
-	RL_LANGS="en,pt"  > /dev/null
-}
-
-rm live_test.tar.gz
-
-echo "Done. Run with: cucumber BASE_URL='<url>'"
+#if [ "$UID" -ne "0"  ] 
+#  then
+#  echo 'You must be root to install Live Test !'
+#  exit
+#fi
+#
+### BEGIN PROGRAM
+#
+#echo "Type the absolut path of the project's root folder:"
+#read PROJECT_ROOT
+#
+#cd $PROJECT_ROOT
+#
+#if [ -d "${PWD}/tests/lib" ]; then
+#  echo 'You already have Live Test installed. Be happy: updating requirements...'  
+#else
+#  echo -e 'Downloading Live Tests...\n'
+#  wget http://www.toetec.com.br/live_test.tar.gz  
+#  echo -e 'Unpacking the file...\n'
+#  tar -xzvf live_test.tar.gz  
+#  echo -e 'Installing requirements...\n'
+#fi
+#
+#cd tests
+#
+#apt-get update &> /dev/null
+#
+#hash ruby1.9.3 &> /dev/null || {
+#	echo '+ Installing Ruby1.9.3'
+#	apt-get install -y ruby1.9.3 > /dev/null
+#}
+#
+#hash xvfb &> /dev/null || {
+#	echo '+ Installing xvfb'
+#	apt-get install -y xvfb > /dev/null
+#}
+#
+#hash firefox &> /dev/null || {
+#	echo '+ Installing Firefox'
+#	apt-get install -y firefox > /dev/null
+#}
+#
+#hash make &> /dev/null || {
+#	echo '+ Installing Firefox'
+#	apt-get install -y make > /dev/null
+#}
+#
+#hash bundler &> /dev/null || {
+#	echo '+ Installing bundler'
+#	gem install bundler  > /dev/null
+#}
+#
+#hash bundle &> /dev/null || {
+#	echo '+ Installing required gems...'
+#	bundle install  > /dev/null
+#}
+#
+#hash rllangs &> /dev/null || {
+#	echo '+ Setting i18n support...'
+#	RL_LANGS="en,pt"  > /dev/null
+#}
+#
+#rm live_test.tar.gz
+#
+#echo "Done. Run with: cucumber BASE_URL='<url>'"
